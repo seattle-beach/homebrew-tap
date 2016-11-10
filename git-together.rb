0c1abd335675b167855f9ea88c1fccb2ce8db09e
Alpha Chen <alpha.chen@gmail.com>
2016-11-09 17:58:29 -0800

    bump git-together to 0.1.0-alpha.2

--- git-together.rb
+++ git-together.rb
@@ -1,9 +1,9 @@
 class GitTogether < Formula
   desc "git-together"
   homepage "https://github.com/kejadlen/git-together"
-  url "https://github.com/kejadlen/git-together/releases/download/v0.1.0-alpha.1/git-together"
-  sha256 "781d8f14be6ca843eb29c8818d69faf8d9d1d3589f4ff6b3d3a37e4ed4cb7df7"
-  version "0.1.0-alpha.1"
+  url "https://github.com/kejadlen/git-together/releases/download/v0.1.0-alpha.2/git-together"
+  sha256 "2eed964635460782b05784949f03020c4bf9403a3e5f2d2f14833cec11ab6e2d"
+  version "0.1.0-alpha.2"
 
   def install
     bin.install "git-together"
