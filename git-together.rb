8f5b8d8a32077345fbb65dd86f7f32ff513d4a49
Alpha Chen <alpha.chen@gmail.com>
2016-11-11 10:37:34 -0800

    git-together -> v0.1.0-alpha.4

--- git-together.rb
+++ git-together.rb
@@ -1,9 +1,11 @@
 class GitTogether < Formula
+  VERSION = "0.1.0-alpha.4"
+
   desc "git-together"
   homepage "https://github.com/kejadlen/git-together"
-  url "https://github.com/kejadlen/git-together/releases/download/v0.1.0-alpha.2/git-together"
+  url "https://github.com/kejadlen/git-together/releases/download/#{VERSION}/git-together"
   sha256 "2eed964635460782b05784949f03020c4bf9403a3e5f2d2f14833cec11ab6e2d"
-  version "0.1.0-alpha.2"
+  version VERSION
 
   depends_on "libssh2" => :run
 
