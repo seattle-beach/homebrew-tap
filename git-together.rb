19596938b8b2ebe626399db93c7c745157eb9931
Alpha Chen <alpha.chen@gmail.com>
2016-11-11 10:38:43 -0800

    fix SHA for git-together

--- git-together.rb
+++ git-together.rb
@@ -4,7 +4,7 @@ class GitTogether < Formula
   desc "git-together"
   homepage "https://github.com/kejadlen/git-together"
   url "https://github.com/kejadlen/git-together/releases/download/v#{VERSION}/git-together"
-  sha256 "2eed964635460782b05784949f03020c4bf9403a3e5f2d2f14833cec11ab6e2d"
+  sha256 "93e91abcc8b19686154b3aaae40209fdab398c585d17d85fca74e75063a55e1b"
   version VERSION
 
   depends_on "libssh2" => :run
