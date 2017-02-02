4bd7bc8621b98b80be6c1ad88740131f39c7c359
Alpha Chen <alpha.chen@gmail.com>
2017-02-02 12:43:47 -0800

    bump git-together to v0.1.0-alpha.10

--- git-together.rb
+++ git-together.rb
@@ -1,10 +1,10 @@
 class GitTogether < Formula
-  VERSION = "0.1.0-alpha.7"
+  VERSION = "0.1.0-alpha.10"
 
   desc "git-together"
   homepage "https://github.com/kejadlen/git-together"
-  url "https://github.com/kejadlen/git-together/releases/download/v#{VERSION}/git-together"
-  sha256 "df968dcf811e95c3aba2662e45b72f062e9549a39db62eb896691fc17fb7eb1f"
+  url "https://github.com/kejadlen/git-together/releases/download/v#{VERSION}/git-together-v#{VERSION}-x86_64-apple-darwin.tar.gz"
+  sha256 "0b7b4328798b89c09e4579e96835fc3217370cee563bd621f3fd9c7f41a8935e"
   version VERSION
 
   depends_on "libssh2" => :run
